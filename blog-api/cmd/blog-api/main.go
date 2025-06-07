08161a93e923ae8b4baed50f6580b5bc96128d52
vinit-jpl <vinit.jpl@gmail.com>
2025-06-07 23:16:51 +0530

    serve created

--- blog-api/cmd/blog-api/main.go
+++ blog-api/cmd/blog-api/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func test() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+}
 func main() {
+
+	// load the enviromnent variables from the .env file
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+
+	}
+
+	test()
+	port := os.Getenv("PORT")
+	// fmt.Println("port from .env file:", port)
+
+	fmt.Println("server started on port:", port)
+
+	http.ListenAndServe("localhost:"+port, nil)
 
 }
