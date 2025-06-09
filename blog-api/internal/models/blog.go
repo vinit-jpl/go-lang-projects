3d2c6a5eeccfbe48ddfafa05ee55be26ae4157ad
vinit-jpl <vinit.jpl@gmail.com>
2025-06-09 18:48:06 +0530

    new blog api started

---
+++ blog-api/internal/models/blog.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Blogpost struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"created_at"`
+}
