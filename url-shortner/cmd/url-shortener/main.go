c747818de15a9e1ea691b1d3fdd9f3a9933c2ac1
vinit-jpl <vinit.jpl@gmail.com>
2025-09-25 12:33:12 +0530

    main.go refactored

--- url-shortner/cmd/url-shortener/main.go
+++ url-shortner/cmd/url-shortener/main.go
@@ -15,62 +15,62 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	// Load env variales
-
-	err := godotenv.Load()
-
-	if err != nil {
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+}
 
+func setupServer() *http.Server {
+	// Get port
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	// Initialze gorm database connection
-
+	// Init DB
 	db := database.New()
 
-	// Initialize gin router
-
+	// Init Router
 	router := gin.Default()
-
-	// Register routes
 	routes.SetupRoutes(router, db)
 
-	// Create http server
-
-	srv := &http.Server{
+	// Return HTTP Server
+	return &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
 	}
+}
 
-	// start the server in go routine
+func startServer(srv *http.Server) {
 	go func() {
-		log.Println("Server started on port:", port)
+		log.Println("Server started on port", srv.Addr)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("ListenAndServe failed: %v", err)
 		}
 	}()
+}
 
-	// Graceful shutdown
+func gracefulShutdown(srv *http.Server) {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
 	<-quit
 	log.Println("Shutting down server...")
 
-	// shutdown context with timeout
-
-	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
-
-	defer cancle()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("Server forced to shutdown: %v", err)
 	}
 
-	log.Println("Server exiting gracefully")
+	log.Println("Server exited gracefully")
+}
 
+func main() {
+	loadEnv()
+	srv := setupServer()
+	startServer(srv)
+	gracefulShutdown(srv)
 }
